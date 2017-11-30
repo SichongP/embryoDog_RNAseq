6d9615c1892ac7de33101b04d55ccedb010f6aa1
Sichong <scpeng@ucdavis.edu>
2017-11-30 11:33:31 -0800

    perl script to align RNAseq

---
+++ hisat2_BD342.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+#system(module load hisat);
+my $r1, $r2;
+open(my $r1file,"R1.txt") or die "Can't open R1.txt";
+open(my $r2file,"R2.txt") or die "Can't open R2.txt";
+#print $r2file;
+#$r1=<$r1file>; print $r1;
+$r1=<$r1file>;$r2=<$r2file>;
+my $BD342_1,$BD342_2,$BD410_1,$BD410_2;
+while($r1)
+{
+chomp $r1; chomp $r2;
+        if($r1=~m/BD342/g)
+        {
+                $BD342_1=$BD342_1.$r1.",";
+                $BD342_2=$BD342_2.$r2.",";
+        }
+        else
+        {
+                $BD410_1=$BD410_1.$r1.",";
+                $BD410_2=$BD410_2.$r2.",";
+        }
+        $r1=<$r1file>;$r2=<$r2file>;
+}
+#print $BD342_1;
+system("hisat2","--summary-file BD342.summary","-p 20","-x /data/embryoDog/UCSC_ref/Dog_ref","-1 ${BD342_1}","-2 ${BD342_2}","-S BD342.sam");
+
+close($r2file);
+close($r1file);
