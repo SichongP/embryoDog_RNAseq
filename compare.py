a9d53947323f0b5933055ccb3e1f4edf95027311
scpeng@ucdavis.edu <scpeng@ucdavis.edu>
2017-12-04 21:32:10 +0000

    updated script with clear comments and help info

--- compare.py
+++ compare.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 #above line for running on unix
-#use sys.argv before implementing
 import re
 import sys
-#file_list = ["ann.txt", "unref.txt","output.txt"]
 sys.argv.pop(0)
+#Make sure system arguments are correct or print usage info
 if len(sys.argv) != 3:
 	print(sys.argv)
-	print("usage: compare file1 file2 output")
+	print("usage: compare annotation.gtf novel_transcript_file output.gtf")
 	sys.exit()
 file_list=sys.argv
 unref = open(file_list[1], 'r')
@@ -15,17 +14,21 @@ ann = open(file_list[0], 'r')
 output = open(file_list[2], 'w')
 ids=set()
 for line in unref:
+	#skip header line if existed
 	if "ref_gene_id" in line:
 		continue
 	values = line.split('\t')
 	transcript_id = values[4]
 	ids.add(transcript_id)
 for line in ann:
+	#skip comments in gtf file
 	if "#" in line:
 		continue
 	values = line.split('\t')
+	#skip exon records
 	if values[2] != "transcript":
 		continue
+	#get transcript_id of each transcript record
 	match = re.search("transcript_id \"(.*?)\";",values[8])
 	if match:
 		transcript_id=match.group(1)
